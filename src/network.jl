6d6c318fad7d1824573225b8fac86bb703b07595
Dennis Wilson <dennis@d9w.xyz>
2018-05-23 04:21:45 +0200

    network with no layers, more realistic lif neurons

--- src/network.jl
+++ src/network.jl
@@ -1,93 +1,105 @@
-struct Layer
+struct Network
     neurons::Array{Float64}
     weights::Array{Float64}
-    inhib::Array{Float64}
+    ge::Array{Float64}
+    gi::Array{Float64}
     trace::Array{Float64}
-    spikes::BitArray
-end
-
-function Layer(nin::Int64, nout::Int64, cfg::STDPConfig, rng::MersenneTwister)
-    neurons = zeros(4, nout)
-    neurons[1, :] = cfg.vstart
-    neurons[2, :] = 1.0
-    neurons[3, :] = 0.2*cfg.vstart
-    weights = min.(1.0, max.(0.0, cfg.weight_std.*randn(rng, nin, nout)
-                             .+ cfg.weight_mean))
-    inhib = cfg.inhib_weight * (1.0 - eye(nout, nout))
-    trace = zeros(nin, nout)
-    Layer(neurons, weights, inhib, trace, falses(nout))
-end
-
-struct Network
-    n_input::Int64
-    n_hidden::Int64
-    n_output::Int64
+    excitatory::BitArray
+    inputs::BitArray
+    outputs::BitArray
+    da::Float64
+    t::Int64
+    nn::Int64
     cfg::STDPConfig
-    layers::Array{Layer}
+    rng::MersenneTwister
 end
 
-function Network(n_input::Int64, n_hidden::Int64, n_output::Int64,
-                 cfg::STDPConfig; rng::MersenneTwister=MersenneTwister(0))
-    hidden = Layer(n_input, n_hidden, cfg, rng)
-    output = Layer(n_hidden, n_output, cfg, rng)
-    Network(n_input, n_hidden, n_output, cfg, [hidden, output])
+function Network(n_neurons::Int64, n_input::Int64, n_output::Int64,
+                 cfg::STDPConfig; rng::MersenneTwister=MersenneTwister(0),
+                 n_exc::Int64 = Int64(round(0.8 * n_neurons)))
+    # starting membrane threshold for all neurons
+    neurons = zeros(n_neurons, 1)
+    neurons[:, 1] = cfg.vstart
+    # index of excitatory neurons
+    n_inh = n_neurons - n_exc
+    excitatory = [trues(n_exc); falses(n_inh)]
+    shuffle!(excitatory)
+    # set weights
+    weights = rand(n_neurons, n_neurons)
+    weights[~.(excitatory), :] = 1.0
+    weights .*= (1.0 - eye(n_neurons, n_neurons))
+    # set input neurons
+    inputs = falses(n_neurons)
+    inds = shuffle(find(excitatory))
+    inputs[inds[1:n_input]] = true
+    # set output neurons
+    inds = shuffle(find(excitatory .& .~(inputs)))
+    outputs[inds[1:n_output]] = true
+    tspike = zeros(n_neurons, n_neurons)
+    ge = zeros(n_neurons, n_neurons)
+    gi = zeros(n_neurons, n_neurons)
+    da = 1.0
+    Network(neurons, weights, ge, gi, tspike, excitatory, inputs, outputs,
+            1.0, 0, n_neurons, cfg, rng)
 end
 
-function spike!(network::Network, layer::Layer, input::BitArray)
-    inputs = (input' * layer.weights) - (layer.spikes' * layer.inhib)
-    inputs = max.(-1.0, min.(1.0, inputs))
-    for n in 1:size(layer.neurons, 2)
-        nstate = [layer.neurons[:, n]; inputs[n]]
-        fout = network.cfg.neuron_function(nstate)
-        layer.neurons[:, n] .+= fout[1:4]
-        layer.spikes[n] = fout[5] > 0.0
-    end
-    layer.neurons[layer.neurons .< -1.0] .= -1.0
-    layer.neurons[layer.neurons .> 1.0] .= 1.0
+function spike!(n::Network, input_spikes::BitArray)
+    # problem input
+    inds = find(n.inputs)[find(input_spikes)]
+    n.neurons[inds, 1] += n.cfg.vinput
+    # calculate spikes, reset spiked pre-synaptic membranes
+    spikes = n.neurons[:, 1] .>= n.cfg.vthresh
+    n.tspike[spikes] = n.t
+    n.neurons[spikes, 1] = n.cfg.vreset
+    # calculate conductance
+    espikes = spikes .& n.excitatory
+    n.ge[espikes, :] += n.weights[espikes, :]
+    ispikes = spikes .& .~(n.excitatory)
+    n.gi[ispikes, :] += n.weights[ispikes, :]
+    # apply post-synaptic membrane delta
+    v = neurons[:, 1]
+    n.neurons[:, 1] += ((n.cfg.eleak - v) + (sum(n.ge, 1)' .* (n.cfg.eexc - v))
+                        + (sum(n.gi, 1)' .* (n.cfg.einh - v))
+                        + n.cfg.thalamic * randn(n.rng, n.nn)) / n.cfg.tm
+    n.ge -= n.ge / n.cfg.te
+    n.gi -= n.gi / n.cfg.ti
+    n.t += 1
+    spikes
+end
+
+function learn!(n::Network, spikes::BitArray)
+    n.trace[spikes, :] .+= 1
+    dw = (n.cfg.lr * n.da * (n.trace - n.cfg.target) .*
+          (n.cfg.wmax - n.weights[:, spikes]).^n.cfg.mu)
+    n.weights[:, spikes] .+= dw
+    n.trace .-= (n.trace / n.cgf.pre_dt)
     nothing
+    # dt = repmat(n.tspike, 1, n.nn) - repmat(n.tspike, 1, n.nn)'
+    # spiked = .|(repmat(spikes, 1, n.nn), repmat(spikes, 1, n.nn)')
+    # # excitatory weight change
+    # exc = repmat(n.excitatory, 1, n.nn)
+    # dtpos = (dt .> 0) .& exc .& spiked
+    # dtneg = (dt .< 0) .& exc .& spiked
+    # n.weights[dtpos] .+= (n.da * n.cfg.lre * exp.(dt[dtpos]/n.cfg.tltpe) * n.cfg.alt
+    #                       * exp.(-n.cfg.cltp*n.weights[dtpos]/n.cfg.w0))
+    # n.weights[dtneg] .+= (n.da * n.cfg.lre * exp.(-dt[dtpos]/n.cfg.tltde) * n.cfg.altd
+    #                       * exp.(log.(1+(n.weights[dtpos]/n.cfg.w0)*n.cfg.cltd)/
+    #                              log.(1+n.cfg.cltd)))
+    # # inhibitory weight change
+    # inh = repmat(~.(n.excitatory), 1, n.nn)
+    # dtpos = (dt .> 0) .& inh .& spiked
+    # dtneg = (dt .< 0) .& inh .& spiked
+    # n.weights[dtpos] .+= (n.da * n.cfg.lri * n.cfg.ip * exp.(-dt[dtpos]/n.cfg.tposti))
+    # n.weights[dtneg] .+= (n.da * n.cfg.lri * n.cfg.iq * exp.(dt[dtpos]/n.cfg.tprei))
+    # n.weights[inh .& spiked] -= n.da * n.cfg.lri * n.cfg.alpha
+    # n.da *= (1.0 - n.cfg.dopamine_absorption)
+    # nothing
 end
 
-function learn!(network::Network, layer::Layer, input_spikes::BitArray)
-    cfg = network.cfg
-    layer.trace[input_spikes, :] .+= cfg.pre_inc
-    layer.trace .-= (layer.trace ./ cfg.pre_dt)
-    layer.weights[:, layer.spikes] .+= cfg.stdp_lr .* (
-        layer.trace[:, layer.spikes] .- cfg.pre_target) .* (
-            (cfg.wmax .- layer.weights[:, layer.spikes]).^cfg.stdp_mu)
-    layer.weights[layer.weights .< 0] .= 0.0
-    nothing
-end
-
-function step!(network::Network, input_spikes::BitArray, train::Bool)
-    for l in eachindex(network.layers)
-        spike!(network, network.layers[l], input_spikes)
-        if train
-            learn!(network, network.layers[l], input_spikes)
-        end
-        input_spikes = copy(network.layers[l].spikes)
+function step!(n::Network, input_spikes::BitArray, train::Bool)
+    spikes = spike!(n, input_spikes)
+    if train
+        learn!(n, spikes)
     end
-    network.layers[end].spikes
-end
-
-function iterate!(network::Network, X::Array{Float64}, cfg::STDPConfig,
-                  train::Bool; rng::MersenneTwister=MersenneTwister(0))
-    labels = zeros(Int64, size(X, 2))
-    for x in eachindex(labels)
-        xfr = X[:, x] * cfg.fr
-        out_spikes = zeros(network.n_output)
-        input_spikes = rand(rng, network.n_input, cfg.t_train) .< (cfg.dt * xfr)
-        for t in 1:cfg.t_train
-            out_spikes += step!(network, input_spikes[:, t], train)
-        end
-        labels[x] = indmax(out_spikes)
-        #if x > 10 && sum(out_spikes) == 0
-        #   break
-        #end
-        # blank input
-        input_spikes = falses(network.n_input)
-        for t in 1:cfg.t_blank
-            step!(network, input_spikes, train)
-        end
-    end
-    labels
+    spikes[n.outputs]
 end
